b012be1892bba60ea1a85a6c8d3873ce53e8ec88
jessicashong <jessicashong12@gmail.com>
2022-11-16 16:34:50 -0600

    created files, updated html with basic skeleton, added basic css styling

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+
+
+
+
+//function to search city
+
+//fetch city from weather api
+
+//loop to create button after search
+// function recentSearch(){
+//     for (var i = 0; i < searchedCity.length; i++) {
+//         document.createElement(button)
+//     }
+// }
