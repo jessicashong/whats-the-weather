4fa8992aac0d7614c0e421aa030ed9c797d3b745
jessicashong <jessicashong12@gmail.com>
2022-11-19 18:31:38 -0600

    add function to fetch current and next five days data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,59 @@
 var searchBtn = document.querySelector('.searchBtn');
 var cityInput = document.getElementById('cityInput');
 var cityName = document.getElementById('searchedCity');
-//var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityInput.value + '&appid=b2962e6f6214014ba6bd1dfe349385a6'
+var cityTemp = document.getElementById('temp');
+var cityWind = document.getElementById('wind');
+var cityHumidity = document.getElementById('humidity');
 
-function fetchWeather (city) {
-    //var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city +'&appid=b2962e6f6214014ba6bd1dfe349385a6'
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + city +'&appid=b2962e6f6214014ba6bd1dfe349385a6')
-    .then( function (response) {
-        if (response.ok) {
-            console.log(response)
-            return response.json()
-            .then(function (data) {
-                console.log(data);
-            })
-        } else {
-            alert('Error: ' + response.statusText);
-        }
-    })   
-    .catch(function(error) {
-        alert('Invalid city name.')
-    });
+searchBtn.addEventListener('click', (e) => {fetchCurrentWeather(e)});
+
+function fetchCurrentWeather(e){
+    e.preventDefault()
+    var currentWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+ cityInput.value + '&units=imperial&appid=b2962e6f6214014ba6bd1dfe349385a6'
+    var nextFiveDaysUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityInput.value + '&units=imperial&appid=b2962e6f6214014ba6bd1dfe349385a6'
+    fetch(currentWeatherUrl)
+        .then( function (response) {
+            if (response.ok) {
+                return response.json()}})
+        .then(function (data) {
+                    displayWeatherData(data);
+                })
+        .catch(function(error) {
+            alert('Invalid city name.');
+        });
+      
+    fetch(nextFiveDaysUrl)
+        .then( function (response) {
+            if (response.ok) {
+                return response.json()}})
+        .then(function (data) {
+                    displayWeatherData(data);
+                })
+        .catch(function(error) {
+            alert('Invalid city name.');
+        });
+}
+
+function displayWeatherData(data){
+    console.log(data);
+    var city = data.city.name;
+    var date = data.list[0].dt_txt;
+    var temp = data.list[0].main.temp;
+    var wind = data.list[0].wind.speed;
+    var humidity = data.list[0].main.humidity;
+
+    cityName.innerHTML = city + ' ' + date;
+    cityTemp.innerHTML = 'Temperature: ' + temp + '°F';
+    cityWind.innerHTML = 'Wind: ' + wind + 'mph';
+    cityHumidity.innerHTML = 'Humidity: ' + humidity + '%';
+
+}
+
+function storeInput(){
+    for (var i = 0; cityInput.length; i++){
+        localStorage.setItem('city', cityInput.value);
+
+    }
 }
 //})
 //console.log();
@@ -40,7 +74,6 @@ function fetchWeather (city) {
 //var baseUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
 //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 //https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-//var apiKey = '&appid=b2962e6f6214014ba6bd1dfe349385a6'
 //function to search city formhandler
 
 //fetch city from weather api getCity
